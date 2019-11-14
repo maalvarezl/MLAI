710e7bbbf399aae47a6c2406e062881262c4f08b
Hannah <hannah.zz@qq.com>
2019-11-14 22:17:52 +0000

    my pca and k means

--- myPCA.py
+++ myPCA.py
@@ -51,14 +51,45 @@ class myPCA:
             # show recon img
             plt.subplot(2, number, index + 1)
             plt.title(str(item))
-            plt.imshow(recon_img.renshape(self.h, self.w))
+            plt.imshow(recon_img.reshape(self.h, self.w))
             # show origin img
             plt.subplot(2, number, index + number + 1)
             plt.title(str(item))
-            plt.imshow(x_img.renshape(self.h, self.w))
+            plt.imshow(x_img.reshape(self.h, self.w))
+
+
+class k_means:
+    def __init__(self, k, data):
+        self.k = k
+        self.data = data
+        self.center_points = torch.randn(k, data.shape[1])
+        self.clusters = {}
+        for i in range(k):
+            self.clusters[k] = []
+        print(self.center_points)
+
+    # 做数据分类
+    def doCluster(self):
+        for i in self.data:
+            distance = []
+            for j in self.center_points:
+                distance.append(self.computer_O_diatance(i, j))
+            # 求最小距离下标，分类
+            max_d = max(distance)
+            max_index = distance.index(max_d)
+            # 把这个点分到对应的cluster
+            self.clusters[max_index].append(i)
+        print('do cluster')
+
+    # 计算欧式距离
+    def computer_O_diatance(self, data_point, center_point):
+        re = torch.mul(torch.from_numpy(data_point), center_point)
+        return re
 
 
 if __name__ == '__main__':
+
+    # prepare data
     mnist_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
     train_loader_pca = torch.utils.data.DataLoader(mnist_data, batch_size=4096, shuffle=False)
     X = None
@@ -66,5 +97,24 @@ if __name__ == '__main__':
     for data in train_loader_pca:
         X, y = data
     X = X.view(-1, 28 * 28)  # X.shape : 4096 * 784
-    n_components = 30
+    choose_choose_digits = torch.randint(0, 10, (2,))
+    print("the 2 digit we choose:", choose_choose_digits)
+    i_list = []
+    for i, item in enumerate(y):
+        if item in choose_choose_digits:
+            i_list.append(i)
+    X_2 = X[i_list]
+    y_2 = y[i_list]
+    # 4b
+    my_pca_4 = myPCA(2, X_2, y_2)
+    my_pca_4.doPCA()
+    my_pca_4.showReconImg(10)
 
+    # 要用的数据在这里
+    data = my_pca_4.lower_dimensional_data
+    print(data.shape)
+    print(data[1].shape)
+    e_vectors = my_pca_4.eigenvectors
+    print(e_vectors.shape)
+    kk = k_means(2, data)
+    kk.doCluster()
